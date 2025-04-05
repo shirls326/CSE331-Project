9ea4a06e4c70ac94031191290052f413f73c0641
itsfoolmoon <tituswhang0930@gmail.com>
2025-04-05 17:34:17 -0400

    Changed instructions ui to match figma prototype

--- src/components/CollapseableMenu/Instructions.jsx
+++ src/components/CollapseableMenu/Instructions.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react'
+import { FaChevronDown } from 'react-icons/fa'
 import './instructions.css'
 
 export default function Instructions() {
-  const [isOpen, setIsOpen] = useState(false) // Track visibility
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className='instructionsContainer'>
-        <button 
-            className='collapsable' 
-            onClick={() => setIsOpen(!isOpen)}
-        >
-            <h2>How to play</h2>
-        </button>
-        {isOpen && (
-            <div className='instructionsContent'>
-                <h2>Instructions</h2>
-                <p>1. Click on the tiles to select them.</p>
-                <p>2. Avoid selecting the outlier tiles.</p>
-                <p>3. You have 3 lives. Lose a life for each incorrect selection.</p>
-                <p>4. Win by selecting all non-outlier tiles.</p>
-                <p>5. Good luck!</p>
-            </div>
-        )}
+    <div className='instructionsContainer' onClick={() => setIsOpen(!isOpen)}>
+      <div className="instructionsHeader">
+        <h2>How to play/Instructions</h2>
+        <FaChevronDown className={`chevron ${isOpen ? 'open' : ''}`} />
+      </div>
+      {isOpen && (
+        <div className='instructionsContent'>
+          <hr />
+          <p>1. Click on the tiles to select them.</p>
+          <p>2. Avoid selecting the outlier tiles.</p>
+          <p>3. You have 3 lives. Lose a life for each incorrect selection.</p>
+          <p>4. Win by selecting all non-outlier tiles.</p>
+          <p>5. Good luck!</p>
+        </div>
+      )}
     </div>
   )
 }
