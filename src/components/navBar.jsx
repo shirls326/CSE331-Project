83ac0c1fce8e4f40930db3252e963bf7d87ff362
Shirley Angos <sea325@lehigh.edu>
2025-04-06 16:01:39 -0400

    added the other buttons

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './navBar.css'
+import settings from '../assets/settingsGear.svg'
 
 export default function navBar() {
   return (
     <div className='navBar'>
+      <button>All Games</button>
       <div className='gameTitleLogo'>
         <h3>Theme Thief</h3>
       </div>
+      <button className='roundedButton'>?</button>
+      <button className='roundedButton'><img src = {settings}/></button>
 
     </div>
   )
